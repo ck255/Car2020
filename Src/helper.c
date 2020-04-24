e8fcda82cabe8367b1104e6963aab1075d9e0cc2
Christoph <christoph.krome@web.de>
2020-04-24 20:17:37 +0200

    Initial Version of Car 2020 Project

---
+++ Src/helper.c
@@ -0,0 +1,14 @@
+/*
+ * helper.c
+ *
+ *  Created on: 20.09.2019
+ *      Author: Christoph
+ */
+
+//void convert_to_ascii(uint8_t * p_array, uint8_t size)
+//{
+//	for(int i = 0; i< size; i++)
+//	{
+//
+//	}
+//}
